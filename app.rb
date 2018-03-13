5d27609a285e182de2942b1fafafb0620c6e8827
YanC Lin <yonalin1@gmail.com>
2018-03-13 04:10:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    erb :reversed
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
   end
 
   get '/friends' do
